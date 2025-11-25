c2cf5e0d86a99759048f50b3bef68646b2c03097
Alexander Nestorov <alexandernst@gmail.com>
2025-11-25 01:44:48 +0100

    ++

--- src/mixins/event.py
+++ src/mixins/event.py
@@ -11,10 +11,12 @@ class EventMixin:
         self._events_lock = RLock()
 
     def on(self, event, callback):
+        logger.debug(f"Subscribing to {event=}")
         with self._events_lock:
             self._events.setdefault(event, []).append(callback)
 
     def off(self, event, callback=None):
+        logger.debug(f"Unsubscribing from {event=}")
         with self._events_lock:
             if callback:
                 self._events.get(event, []).remove(callback)
@@ -22,6 +24,7 @@ class EventMixin:
                 self._events.pop(event)
 
     def emit(self, event, *args, **kwargs):
+        logger.debug(f"emitting {event=}")
         # Lock, copy callbacks and unlock so that we don't block the entire
         # world while iterating over the callbacks...
         with self._events_lock:
